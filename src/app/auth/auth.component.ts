430a66d21bff35c02fcf0245e673d2b47357a17e
Shane D. Wilson <shane@fortyau.com>
2020-01-10 09:59:27 -0600

    moved api key into appsettings json

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { AuthService } from './auth.service';
+import { AuthService, AuthResponseData } from './auth.service';
+import { Observable } from 'rxjs';
 
 @Component({
     selector: 'app-auth',
@@ -25,12 +26,17 @@ export class AuthComponent {
         const email = form.value.email;
         const password = form.value.password;
 
+        let authObs: Observable<AuthResponseData>;
+
         this.isLoading = true;
 
         if (this.isLoginMode) {
-
+           authObs = this.authService.login(email, password);
         } else {
-        this.authService.signup(email, password).subscribe(
+            authObs = this.authService.signup(email, password);
+        }
+
+        authObs.subscribe(
             resData => {
                 console.log(resData);
                 this.isLoading = false;
@@ -40,7 +46,7 @@ export class AuthComponent {
                 this.isLoading = false;
             }
         );
-        }
+
         form.reset();
     }
 }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
+import { AppSettings } from '../../../appsettings.json';
 
-interface AuthResponseData {
+export interface AuthResponseData {
     kind: string;
     idToken: string;
     email: string;
     refreshToken: string;
     expiresIn: string;
     localId: string;
+    registered?: boolean;
 }
 
 @Injectable({providedIn: 'root'})
@@ -18,22 +20,37 @@ export class AuthService {
 
     signup(email: string, password: string) {
         return this.http.post<AuthResponseData>(
-            'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyD2lzavqtlIGTo1lj61qcAUsWi6UqBZuF8',
+            'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + AppSettings.firebaseKey,
             {
                 email,
                 password,
                 returnSecureToken: true
             }
-        ).pipe(catchError(errorRes => {
-            let errorMessage = 'An unknown error occurred.';
-            if (!errorRes.error || !errorRes.error.error) {
-                return throwError(errorMessage);
+        )
+        .pipe(catchError(this.handleError));
+    }
+
+    login(email: string, password: string) {
+       return this.http.post<AuthResponseData>(
+           'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + AppSettings.firebaseKey,
+            {
+                email,
+                password,
+                returnSecureToken: true
             }
-            switch (errorRes.error.error.message) {
-                case 'EMAIL_EXISTS':
-                    errorMessage = 'This email already exists.';
-            }
+       )
+       .pipe(catchError(this.handleError));
+    }
+
+    private handleError(errorRes: HttpErrorResponse) {
+        let errorMessage = 'An unknown error occurred.';
+        if (!errorRes.error || !errorRes.error.error) {
             return throwError(errorMessage);
-        }));
+        }
+        switch (errorRes.error.error.message) {
+            case 'EMAIL_EXISTS':
+                errorMessage = 'This email already exists.';
+        }
+        return throwError(errorMessage);
     }
 }
