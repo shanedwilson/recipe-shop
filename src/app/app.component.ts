d5b5ba501908446757ed3763c0ff1db453283363
Shane D. Wilson <shane@fortyau.com>
2020-01-17 10:18:21 -0600

    finished section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from './auth/auth.service';
 import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+import { LoggingService } from './logging.service';
 
 @Component({
   selector: 'app-root',
@@ -8,9 +9,10 @@ import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private loggingService: LoggingService) {}
 
   ngOnInit() {
     this.authService.autoLogin();
+    this.loggingService.printLog('Hello from AppComponent NgOnInit');
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,8 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { RecipesModule } from './recipes/recipes.module';
-import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
-import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
@@ -19,10 +16,7 @@ import { AuthModule } from './auth/auth.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AuthModule,
     HttpClientModule,
-    RecipesModule,
-    ShoppingListModule,
     SharedModule,
     CoreModule
   ],
